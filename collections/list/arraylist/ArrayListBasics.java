b468a35ca926478901c8db7cc2a469304764695d
AmitPundekar16 <222381731+AmitPundekar16@users.noreply.github.com>
2026-07-21 14:05:11 +0530

    Use add method with index and without index

--- collections/list/arraylist/ArrayListBasics.java
+++ collections/list/arraylist/ArrayListBasics.java
@@ -6,10 +6,14 @@ public class ArrayListBasics
     public static void main(String[] args)
     {
         List<Integer> l1=new ArrayList<>();
-
         l1.add(5);
         l1.add(19);
-
         System.out.println(l1);
+       
+        // To add each element at first position the add will shift current first to right and will not remove from list
+        List<String> l2=new ArrayList<>();
+        l2.add(0,"Mumbai");
+        l2.add(0,"pune");
+        System.out.println(l2);
     }
 }
