579e99d7b0f0084cd187139a6c35b3fffcff36b6
AmitPundekar16 <222381731+AmitPundekar16@users.noreply.github.com>
2026-07-21 14:52:39 +0530

    use remove,get,and equals method

--- collections/list/arraylist/ArrayListBasics.java
+++ collections/list/arraylist/ArrayListBasics.java
@@ -12,8 +12,25 @@ public class ArrayListBasics
        
         // To add each element at first position the add will shift current first to right and will not remove from list
         List<String> l2=new ArrayList<>();
-        l2.add(0,"Mumbai");
+        l2.add(0,"MMumbai");
         l2.add(0,"pune");
         System.out.println(l2);
+
+
+        List<String> l3=new ArrayList<>();
+        l3.add(0,"Banana");
+        l3.add(1,"apple");
+        l3.add("mango");
+        l3.add("guava");
+        l3.add("papaya");
+        System.out.println(l3);
+        for (int i = 0; i < l3.size(); i++) {
+          if (l3.get(i).equals("mango")) {
+             l3.remove(i);
+             i--; 
+            }
+        }
+         System.out.println(l3);
+
     }
 }
