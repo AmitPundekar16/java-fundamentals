38687fac027281b03e1e462c1cebbea8541fbd1d
AmitPundekar16 <222381731+AmitPundekar16@users.noreply.github.com>
2026-07-21 13:51:06 +0530

    Perform Basic operation on arraylist

---
+++ collections/list/arraylist/ArrayListBasics.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListBasics
+{
+    public static void main(String[] args)
+    {
+        List<Integer> l1=new ArrayList<>();
+
+        l1.add(5);
+        l1.add(19);
+
+        System.out.println(l1);
+    }
+}
