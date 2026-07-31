eb6b003a9bed040be5870ab1b5c5119500b76908
AmitPundekar16 <222381731+AmitPundekar16@users.noreply.github.com>
2026-07-31 06:05:35 +0530

    done practise how to convert primitive array to Integer array

---
+++ collections/list/arraylist/SortList.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+public class SortList
+{
+    public static void main(String []ar)
+    {
+        int arr[]={5,4,2,1,3};
+        List<Integer> l1=new ArrayList<>();
+        for(int i:arr)
+        {
+            l1.add(i);
+        }
+
+
+        Collections.sort(l1);
+        System.out.println(l1);
+    }
+}
