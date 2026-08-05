22dc824d7acb2c91fbf50f5185eaa3a5fb92e350
AmitPundekar16 <222381731+AmitPundekar16@users.noreply.github.com>
2026-08-05 23:36:58 +0530

    display column wise list

--- collections/list/arraylist/TwoDArrayList.java
+++ collections/list/arraylist/TwoDArrayList.java
@@ -7,15 +7,28 @@ public class TwoDArrayList
     {
         List<List<Integer>> l1=new ArrayList<>();
          l1.add(new ArrayList<>());
+         l1.add(new ArrayList<>());
+         l1.add(new ArrayList<>());
+
         l1.get(0).add(1);
+        l1.get(0).add(2);
+        l1.get(0).add(3);
+
+        l1.get(1).add(4);
+        l1.get(1).add(5);
+        l1.get(1).add(6);
+
+
+        l1.get(2).add(7);
+        l1.get(2).add(8);
+        l1.get(2).add(9);
+
+
         System.out.println(l1);
-        for(int i=0;i<l1.size();i++)
-        {
-            for(int j=0;j<l1.get(i).size();j++)
-            {
-                System.out.print(l1.get(i).get(j)+" ");
-            }
-            System.out.println();
+
+        for(int i=0;i<l1.size();i++){
+            System.out.print(" "+l1.get(i).get(0));
         }
+       
     }
 }
